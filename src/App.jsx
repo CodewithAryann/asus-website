2dca08777e0aa167469be8a3c308fb8e9817046d
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-09-02 15:02:25 +0400

    2

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { Canvas } from '@react-three/fiber';
 import React from 'react';
 import "./style.css";
 import AsusContainer from "./asusContainer"; 
-import { OrbitControls } from '@react-three/drei';
+import { Environment, OrbitControls } from '@react-three/drei';
 
 const App = () => {
   return ( 
     <div className="canvas-container">
-    <Canvas camera={{ fov : 10, position : [10, -2, 120]}} > 
+    <Canvas camera={{ fov : 4, position : [0, 2, 120]}} > 
       <OrbitControls></OrbitControls>
+      <Environment files={["https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_08_4k.exr"]}/>
       <AsusContainer /> 
     </Canvas>
     </div>
--- src/asusContainer.jsx
+++ src/asusContainer.jsx
@@ -2,10 +2,13 @@ import { useGLTF } from '@react-three/drei';
 import React from 'react';
 
 const AsusContainer = () => {
-  let model = useGLTF("./asus_rog.glb");
-  return <primitive style={{}} object={model.scene}/>;  
+  let model = useGLTF("./mac.glb");
+  
+  return (
+    <group position={[0, -1, 120]}>
+      <primitive object={model.scene} />
+    </group>
+  );
 };
 
-
 export default AsusContainer;
-
