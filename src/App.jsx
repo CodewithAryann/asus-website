fd7f6fd53afc7f11c4b614136cc4a4631eba0da5
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-09-04 14:51:51 +0400

    4

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import "./style.css";
 import AsusContainer from "./asusContainer"; 
 import { Environment, OrbitControls, ScrollControls } from '@react-three/drei';
 
+
 const App = () => {
   
   return ( 
+   <div>
     <div className="canvas-container w-full h-screen">
       <div className='text-white absolute top-32 left-1/2 -translate-x-1/2'>
       <h1>AUSU Laptop</h1>
@@ -16,6 +18,10 @@ const App = () => {
         <Environment files="https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_08_4k.exr" />
            <AsusContainer />     
       </Canvas>
+    </div>
+    <div>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, impedit minus consectetur veniam delectus eius accusamus! Aliquid velit vel totam molestias unde rem quasi quia possimus veritatis excepturi. Perspiciatis, sequi.</h1>
+    </div>
     </div>
   );
 }
