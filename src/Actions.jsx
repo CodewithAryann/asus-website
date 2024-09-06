def72db9ebccb006524780d8dbdb19f618fc6031
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-09-06 15:11:35 +0400

    6

--- src/Actions.jsx
+++ src/Actions.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import "./action.css";
 
-const action = () => {
+const Action = () => {
   return (
-    <div>
+    <div className='action-div'>
       <h1>ROG IN ACTION</h1>
+      <div className='action-items'>
+        <div className='action-item'>
+          <img src="./action-1.webp" alt="" />
+          <h3>GAMING</h3>
+          <p>ROG teams up with iconic voice actors in cheeky new video</p>
+        </div>
+        <div className='action-item'>
+          <img src="./action-1.webp" alt="" />
+          <h3>GAMING</h3>
+          <p>ROG teams up with iconic voice actors in cheeky new video</p>
+        </div>
+        <div className='action-item'>
+          <img src="./action-1.webp" alt="" />
+          <h3>GAMING</h3>
+          <p>ROG teams up with iconic voice actors in cheeky new video</p>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default action
+export default Action
