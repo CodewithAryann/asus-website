69f8866dd7dc0e9522e6e1774d945069ee778138
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-09-06 14:18:08 +0400

    6

--- src/living.jsx
+++ src/living.jsx
@@ -1,145 +1,45 @@
 import React from 'react';
-
+import "./living.css";
 const Living = () => {
   return (
     <div>
-      <h1>LIVING FOR GAMERS - ABOUT ROG</h1>
-      <div className="Actions__actionScrollBar__03Dj2">
-        <div className="Actions__actionContent__7kIJU" style={{ width: '1160px' }}>
-          <a
-            href="https://rog.asus.com/me-en/articles/news/get-ahead-of-the-game-with-the-latest-rog-gaming-tech-from-gamescom-2024/"
-            className="ActionItem__actionsItem__do2Lu ActionItem__active__MRVrL ActionItem__whiteText__ZTo8j"
-          >
-            <div className="ActionItem__articleContentImage__WzNvF">
-              <img
-                src="https://dlcdnrog.asus.com/rog/media/1724151926819.webp"
-                role="presentation"
-                alt=""
-                loading="lazy"
-                className="recentStoryImage"
-              />
-            </div>
-            <div className="ActionContent__actionsItemContent__M69iZ ActionContent__whiteText__CgRpc">
-              <p className="ActionContent__actionTitle__TFIhY">
-                Get ahead of the game with the latest ROG gaming tech from Gamescom 2024
-              </p>
-              <span className="ActionContent__actionText__I4+d2">
-                From new X870 motherboards to OLED displays and plenty of mice and keyboards, here’s everything ROG announced at Gamescom 2024.
-              </span>
-            </div>
+        <div className='action'>
+            <h1>LIVING FOR GAMERS - ABOUT ROG</h1>
+            <div className="container"> 
+        <div className="item">
+          <a href="https://rog.asus.com/me-en/articles/news/get-ahead-of-the-game-with-the-latest-rog-gaming-tech-from-gamescom-2024/">
+            <img src="https://dlcdnrog.asus.com/rog/media/1724151926819.webp" alt="Gamescom 2024" />
+            <p>Get ahead of the game with the latest ROG gaming tech from Gamescom 2024</p>
           </a>
-
-          <a
-            href="https://rog.asus.com/me-en/articles/gaming-monitors/qd-oled-and-360hz-converge-in-the-rog-strix-oled-xg27acdng/"
-            className="ActionItem__actionsItem__do2Lu ActionItem__active__MRVrL ActionItem__whiteText__ZTo8j"
-          >
-            <div className="ActionItem__articleContentImage__WzNvF">
-              <img
-                src="https://dlcdnrog.asus.com/rog/media/1723842167221.webp"
-                role="presentation"
-                alt=""
-                loading="lazy"
-                className="recentStoryImage"
-              />
-            </div>
-            <div className="ActionContent__actionsItemContent__M69iZ ActionContent__whiteText__CgRpc">
-              <p className="ActionContent__actionTitle__TFIhY">
-                QD-OLED and 360Hz converge in the ROG Strix OLED XG27ACDNG
-              </p>
-              <span className="ActionContent__actionText__I4+d2">
-                The ROG Strix OLED XG27ACDNG combines a lightning-quick refresh rate, high pixel density, and a next-gen OLED panel.
-              </span>
-            </div>
+        </div>
+        <div className="item">
+          <a href="https://rog.asus.com/me-en/articles/gaming-monitors/qd-oled-and-360hz-converge-in-the-rog-strix-oled-xg27acdng/">
+            <img src="https://dlcdnrog.asus.com/rog/media/1723842167221.webp" alt="OLED XG27ACDNG" />
+            <p>QD-OLED and 360Hz converge in the ROG Strix OLED XG27ACDNG</p>
           </a>
-
-          <a
-            href="https://rog.asus.com/me-en/articles/crosshair-motherboards/the-rog-crosshair-x870e-hero-sets-a-new-standard-for-high-end-gaming-prowess/"
-            className="ActionItem__actionsItem__do2Lu ActionItem__active__MRVrL ActionItem__whiteText__ZTo8j"
-          >
-            <div className="ActionItem__articleContentImage__WzNvF">
-              <img
-                src="https://dlcdnrog.asus.com/rog/media/1723864904277.webp"
-                role="presentation"
-                alt=""
-                loading="lazy"
-                className="recentStoryImage"
-              />
-            </div>
-            <div className="ActionContent__actionsItemContent__M69iZ ActionContent__whiteText__CgRpc">
-              <p className="ActionContent__actionTitle__TFIhY">
-                The ROG Crosshair X870E Hero sets a new standard for high-end gaming prowess
-              </p>
-              <span className="ActionContent__actionText__I4+d2">
-                The ROG Crosshair X870E is one of your most feature-rich, high-end motherboard options for an AMD Ryzen 9000 Series CPU.
-              </span>
-            </div>
+        </div>
+        <div className="item">
+          <a href="https://rog.asus.com/me-en/articles/crosshair-motherboards/the-rog-crosshair-x870e-hero-sets-a-new-standard-for-high-end-gaming-prowess/">
+            <img src="https://dlcdnrog.asus.com/rog/media/1723864904277.webp" alt="X870E Hero" />
+            <p>The ROG Crosshair X870E Hero sets a new standard for high-end gaming prowess</p>
           </a>
-
-          <a
-            href="https://rog.asus.com/me-en/articles/psus/the-rog-strix-platinum-psu-series-provides-unwavering-stability-for-your-demanding-rig/"
-            className="ActionItem__actionsItem__do2Lu ActionItem__horizontal__EGtTI ActionItem__active__MRVrL ActionItem__whiteText__ZTo8j"
-          >
-            <div className="ActionItem__articleContentImage__WzNvF">
-              <img
-                src="https://dlcdnrog.asus.com/rog/media/1723518686811.webp"
-                role="presentation"
-                alt=""
-                loading="lazy"
-                className="recentStoryImage"
-              />
-            </div>
-            <div className="ActionContent__actionsItemContent__M69iZ ActionContent__whiteText__CgRpc">
-              <p className="ActionContent__actionTitle__TFIhY">
-                The ROG Strix Platinum PSU series provides unwavering stability for your demanding rig
-              </p>
-              <span className="ActionContent__actionText__I4+d2">
-                The ROG Strix Platinum Series is the best PSU for mainstream gaming PCs that demand quality power.
-              </span>
-            </div>
+        </div>
+        <div className="item">
+          <a href="https://rog.asus.com/me-en/articles/psus/the-rog-strix-platinum-psu-series-provides-unwavering-stability-for-your-demanding-rig/">
+            <img src="https://dlcdnrog.asus.com/rog/media/1723518686811.webp" alt="PSU Series" />
+            <p>The ROG Strix Platinum PSU series provides unwavering stability for your demanding rig</p>
           </a>
-
-          <a
-            href="https://rog.asus.com/me-en/articles/gaming-headsets/wireless-freedom-in-esports-ready-cans-meet-the-rog-delta-ii-gaming-headset/"
-            className="ActionItem__actionsItem__do2Lu ActionItem__horizontal__EGtTI ActionItem__active__MRVrL ActionItem__whiteText__ZTo8j"
-          >
-            <div className="ActionItem__articleContentImage__WzNvF">
-              <img
-                src="https://dlcdnrog.asus.com/rog/media/17231648226.webp"
-                role="presentation"
-                alt=""
-                loading="lazy"
-                className="recentStoryImage"
-              />
-            </div>
-            <div className="ActionContent__actionsItemContent__M69iZ ActionContent__whiteText__CgRpc">
-              <p className="ActionContent__actionTitle__TFIhY">
-                Wireless freedom in esports-ready cans: Meet the ROG Delta II gaming headset
-              </p>
-              <span className="ActionContent__actionText__I4+d2">
-                Forget cord hassle with the ROG Delta II gaming headset. Enjoy a comfortable, balanced listening experience that’ll give you an edge over your rivals.
-              </span>
-            </div>
+        </div>
+        <div className="item">
+          <a href="https://rog.asus.com/me-en/articles/gaming-headsets/wireless-freedom-in-esports-ready-cans-meet-the-rog-delta-ii-gaming-headset/">
+            <img src="https://dlcdnrog.asus.com/rog/media/17231648226.webp" alt="Delta II Gaming Headset" />
+            <p>Wireless freedom in esports-ready cans: Meet the ROG Delta II gaming headset</p>
           </a>
         </div>
       </div>
-      <div className="Actions__moreButtons__ymVlM">
-        <a
-          href="https://rog.asus.com/me-en/tag/product"
-          tabIndex="0"
-          aria-label="LEARN MORE article tag product"
-          className="MoreButton__moreButton__c16n8 MoreButton__largerFontSize__BmxsE MoreButton__isBlackBackground__ZbmFL"
-        >
-          <div className="MoreButton__linkIcon__-jKNv">
-            LEARN MORE
-            <span style={{ width: 0, height: 0, opacity: 0 }}>article tag product</span>
-            <svg xmlns="http://www.w3.org/2000/svg" data-name="圖層 1" viewBox="0 0 32 32" role="presentation" focusable="false">
-              <path d="M23.07 13.77L9.06 0v6.7l9.18 8.94-9.18 9.66V32l14.01-14.49 1.87-1.87-1.87-1.87z"></path>
-            </svg>
-          </div>
-        </a>
-      </div>
+        </div>   
     </div>
   );
-};
+}
 
 export default Living;
