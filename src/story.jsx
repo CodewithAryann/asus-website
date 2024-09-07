fb169ce09066511e38b870a6289a0403720b61b4
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-09-07 13:27:50 +0400

    7

--- src/story.jsx
+++ src/story.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const story = () => {
   return (
-    <div style={{ marginTop:"2vw"}}>
+    <div style={{ marginTop:"2vw", marginBottom:"-6px"}}>
       <img style={{width:"100%"}} src="./story.png" alt="" />
     </div>
   )
